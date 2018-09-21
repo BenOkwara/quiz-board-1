a2d90021416207783a8e9f7c3ea5952484059b88
Marvin Chomba <marvin.chomba24@gmail.com>
2018-09-21 20:07:18 +0300

    Add the JS user ineterface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 var sum = 0;
+var userName = $("#name").val();
 $(function() {
   
   $("#start-btn").click(function(event) {
+    var userName = $("#name").val();
     event.preventDefault();
-    $("#form1").show();
-    $("#intro").hide();
+    console.log(userName);
+    if(userName.length === 0) {
+      $("#no-name").text("Please enter your name");
+      $("#name").addClass("enter-name");
+    } else {
+      $("#form1").show();
+      $("#intro").hide();
+    }
   })
   $("#arrow-right").click(function() {
     $("#form1").hide();
@@ -22,5 +30,16 @@ $(function() {
 })
 
 $(document).ready(function() {
-  
-})
+  $("#submit").click(function() {
+    var names = ["js", "year", "framework","function","dom","link"];
+    names.forEach(function(name) {
+      var userValue = $("input:radio[name=" + name + "]:checked").val();
+      if(userValue === "10") {
+        sum+=10;
+      }
+    });
+    var userName = $("#name").val();
+    $(".name-display").text(userName);
+    $(".scoreDisplay").text(sum + "/60");
+  });
+});
